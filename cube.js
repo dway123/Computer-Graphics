76e9b5c152694fbfda5f66d70ac4a4a2ace43a2b
Darwin Huang <huangdarwin2@gmail.com>
2017-03-29 21:23:45 -0400

    1 turn only
    2 turns and it fails.
    Need to update position of cube center after rotation...

--- cube.js
+++ cube.js
@@ -32,14 +32,13 @@ const xAxis = 0;
 const yAxis = 1;
 const zAxis = 2;
 
-var axis = 0;
+var rotationAxis = 0;
 var rotationSign = 1;	//1 for positive, -1 for negative
+var rotationLocation;
 var rotationSpeed = 1;
-var anglesRotated = 0;
-var theta = [0, 0, 0];
+var anglesRotated = 90;
 
 //transformation matrices
-var _rotationMatrix; 
 var _modelViewMatrix;
 var _projectionMatrix;
 
@@ -51,7 +50,7 @@ const baseCubeVertices = [			//these vertices define unit cube, from which all o
     vec4(-0.5, -0.5, -0.5, 1.0),	//4
     vec4(-0.5,  0.5, -0.5, 1.0),	//5
     vec4( 0.5,  0.5, -0.5, 1.0),	//6
-    vec4( 0.5, -0.5, -0.5, 1.0)	//7
+    vec4( 0.5, -0.5, -0.5, 1.0)		//7
 ];
 
 const vertexColors = [
@@ -64,22 +63,13 @@ const vertexColors = [
 ];
 
 //Cubie function! 27 cubies in the rubik's cube
-function Cubie(i, j, k, leftColor, rightColor, upColor, downColor, frontColor, backColor){
+function Cubie(i, j, k){
 	const cubePadding = .1;
-	this.i = i;						//ith index cubie from furthest left on x axis
-	this.j = j;						//jth index cubie from furthest bottom on y axis
-	this.k = k;						//kth index cubie from furthest front on z axis
-	this.ownTheta = [0,0,0];		//rotation of cubie about it's origin (likely shouldn't change)
-	this.scale = [1,1,1];			//scaling of cubie about it's origin (likely shouldn't change)
-	this.rubiksCubeTheta = [0,0,0];	//rotation of cubie about origin of rubik's cube. Used for rotating faces
-
-	this.leftColor = leftColor;		//color of left plane
-	this.rightColor = rightColor;	//color of right plane
-	this.upColor = upColor;			//...
-	this.downColor = downColor;
-	this.frontColor = frontColor;
-	this.backColor = backColor;		//color of back plane
-	
+	this.location = vec4(i,j,k,1.0);		//ith/jth/kth index cubie from furthest left/bottom/front on x/y/z axis (changes per rotation)
+	this.originalLocation = vec3(i,j,k,1.0);//original values for i, j, and k in solved rubik's cube
+	this.ownTheta = [0,0,0];			//rotation of cubie about it's origin (likely shouldn't change)
+	this.scale = [1,1,1];				//scaling of cubie about it's origin (likely shouldn't change)
+	this.rubiksCubeTheta = [0,0,0];		//rotation of cubie about origin of rubik's cube. Used for rotating faces
 
 	this.getModelMatrix = function(){
 		var translationMatrix = translate(i + Math.sign(i) * cubePadding, j + Math.sign(j) * cubePadding, k + Math.sign(k) * cubePadding);
@@ -91,13 +81,31 @@ function Cubie(i, j, k, leftColor, rightColor, upColor, downColor, frontColor, b
         var overallModelMatrix = mult(rubiksCubeRotationMatrix, cubieModelMatrix);
         return overallModelMatrix;
 	}
+
+	this.updateLocation = function(axis, direction){
+		var rotationMatrix = rotateX(90);
+		console.log(this.location);
+		this.location = mult(rotationMatrix,this.location);
+		console.log(this.location);
+	}
 }
 
 function RubiksCube(){
 	this.cubies = [];
+
+	this.initCubies = function(){
+		for(var i = -1; i <= 1; i++){
+	        for(var j = -1; j <= 1; j++){
+	            for(var k = -1; k <= 1; k++){
+	                var newCubie = new Cubie(i, j, k);
+	                this.cubies.push(newCubie);
+	            }
+	        }
+	    }
+	}
 }
 
-var RCube = new RubiksCube();
+var rCube = new RubiksCube();
 
 window.onload = function init()
 {
@@ -115,7 +123,9 @@ window.onload = function init()
     gl.viewport(0, 0, canvas.width, canvas.height);
     gl.clearColor(1.0, 1.0 , 1.0, 1.0);
 
-    gl.enable(gl.DEPTH_TEST);
+    gl.enable(gl.DEPTH_TEST);	//depth visualization
+    // gl.enable(gl.CULL_FACE);	//cull faces
+    // gl.cullFace(gl.FRONT);
 
     //
     //  Load shaders and initialize attribute buffers
@@ -139,86 +149,115 @@ window.onload = function init()
     gl.vertexAttribPointer(vPosition, 4, gl.FLOAT, false, 0, 0);
     gl.enableVertexAttribArray(vPosition);
 
-    _rotationMatrix = gl.getUniformLocation(program, "rotationMatrix");
     _modelViewMatrix = gl.getUniformLocation(program, "modelViewMatrix");
     _projectionMatrix = gl.getUniformLocation(program, "projectionMatrix");
 
     eventListenerSetup();
 
-    initCubies();
+    rCube.initCubies();
 
     render();
 }
 
-function initCubies(){
-	for(var i = -1; i <= 1; i++){
-        for(var j = -1; j <= 1; j++){
-            for(var k = -1; k <= 1; k++){
-                var newCubie = new Cubie(
-                	i, 
-                	j, 
-                	k, 
-                	vertexColors[0], 
-                	vertexColors[1], 
-                	vertexColors[2],
-                	vertexColors[3],
-                	vertexColors[4],
-                	vertexColors[5]
-                );
-                RCube.cubies.push(newCubie);
-            }
-        }
-    }
-}
-
 function eventListenerSetup(){
-    document.getElementById( "pxButton" ).onclick = function () {
-        rotateCubeBy(xAxis, 1);
+	//left/middle/right
+	document.getElementById("lButton").onclick = function () {
+        rotateCube(xAxis, -1, -1);
     };
-    document.getElementById( "nxButton" ).onclick = function () {
-        rotateCubeBy(xAxis, -1);
+    document.getElementById("mButton").onclick = function () {
+        rotateCube(xAxis, -1, 0);
     };
-    document.getElementById( "pyButton" ).onclick = function () {
-        rotateCubeBy(yAxis, 1);
+    document.getElementById("rButton").onclick = function () {
+        rotateCube(xAxis, 1, 1);
     };
-    document.getElementById( "nyButton" ).onclick = function () {
-        rotateCubeBy(yAxis, -1);
+
+    //up/e/down
+    document.getElementById("uButton").onclick = function () {
+        rotateCube(yAxis, -1, 1);
     };
-    document.getElementById( "pzButton" ).onclick = function () {
-        rotateCubeBy(zAxis, 1);
+    document.getElementById("eButton").onclick = function () {
+        rotateCube(yAxis, -1, 0);
     };
-    document.getElementById( "nzButton" ).onclick = function () {
-        rotateCubeBy(zAxis, -1);
+    document.getElementById("dButton").onclick = function () {
+        rotateCube(yAxis, 1, -1);
     };
+
+    //front/slice/back
+    document.getElementById("fButton").onclick = function () {
+        rotateCube(zAxis, -1, -1);
+    };
+    document.getElementById("sButton").onclick = function () {
+        rotateCube(zAxis, -1, 0);
+    };
+    document.getElementById("bButton").onclick = function () {
+        rotateCube(zAxis, 1, 1);
+    };
+
+    //left/middle/right transpose
+    document.getElementById("ltButton").onclick = function () {
+        rotateCube(xAxis, 1, -1);
+    };
+    document.getElementById("mtButton").onclick = function () {
+        rotateCube(xAxis, 1, 0);
+    };
+    document.getElementById("rtButton").onclick = function () {
+        rotateCube(xAxis, -1, 1);
+    };
+
+    //up/e/down
+    document.getElementById("utButton").onclick = function () {
+        rotateCube(yAxis, 1, 1);
+    };
+    document.getElementById("etButton").onclick = function () {
+        rotateCube(yAxis, 1, 0);
+    };
+    document.getElementById("dtButton").onclick = function () {
+        rotateCube(yAxis, -1, -1);
+    };
+
+    //front/slice/back transpose
+    document.getElementById("ftButton").onclick = function () {
+        rotateCube(zAxis, 1, -1);
+    };
+    document.getElementById("stButton").onclick = function () {
+        rotateCube(zAxis, 1, 0);
+    };
+    document.getElementById("btButton").onclick = function () {
+        rotateCube(zAxis, -1, 1);
+    };
+
     var slider = document.getElementById("slider").onchange = function(event) {
         rotationSpeed = parseInt(event.target.value);
     };
     rotationSpeed = 3;
 }
 
-function cubeAligned(){
-	var cubeAligned = true;
-	theta.forEach(function(element){
-		if(element % 90 != 0){
-			cubeAligned = false;
-		}
-	})
-	return cubeAligned;
+function rotationComplete(){
+	return (anglesRotated >= 90);
 }
 
-function rotateCubeBy(ax, sign){
-	console.log(cubeAligned());
-	if(cubeAligned()){
+function rotateCube(ax, sign, loc){
+	if(rotationComplete()){
 		anglesRotated = 0;
-		rotationSign = sign;
 		if(ax === xAxis || ax === yAxis || ax === zAxis){
-			axis = ax;
+			rotationAxis = ax;
 		}
 		else{
-			console.log("ERROR: Invalid Axis!");
+			alert("ERROR: Invalid Axis! Must be 0, 1, or 2");
+		}
+		if(rotationSign === 1 || rotationSign === -1){
+			rotationSign = sign;
+		}
+		else{
+			alert("ERROR: invalid rotation sign! Must be +1 or -1");
+		}
+		if(loc === -1 || loc === 0 || loc === 1){
+			rotationLocation = loc;
+		}
+		else{
+			alert("ERROR: invalid rotation location! Must be -1, 0, or 1");
 		}
 	}
-	
 }
 
 function colorCube()
@@ -231,9 +270,7 @@ function colorCube()
     quad( 6, 5, 1, 2 ); //+y top face 		//cyan
 }
 
-// We need to parition the quad into two triangles in order for
-// WebGL to be able to render it.  In this case, we create two
-// triangles from the quad indices
+// Parition quad into two triangles from quad indices
 function quad(a, b, c, d)
 {
     //vertex color assigned by the index of the vertex
@@ -248,37 +285,40 @@ function quad(a, b, c, d)
 function render()
 {
     gl.clear( gl.COLOR_BUFFER_BIT | gl.DEPTH_BUFFER_BIT);
-    anglesRotated += rotationSpeed;
-    if(anglesRotated <= 90){
-    	theta[axis] += rotationSpeed * rotationSign;
+    
+    //during rotation, rotate all relevant cubies, as noted by their location 
+   if(!rotationComplete()){
+    	rCube.cubies.forEach(function(cubie){
+    		if(cubie.location[rotationAxis] == rotationLocation){
+	    		cubie.rubiksCubeTheta[rotationAxis] += rotationSpeed * rotationSign;
+	    	}
+    	})
     }
+    if(anglesRotated === 90){//just finished rotation
+    	rCube.cubies.forEach(function(cubie){
+    		if(cubie.location[rotationAxis] == rotationLocation){
+    			//if()
+    		}
+    	});
+    }
+    if(anglesRotated <= 90){
+ 		anglesRotated += rotationSpeed;
+	}
     colorCube();
 
 
-    var rotationMatrix, modelViewMatrix, projectionMatrix;
-    rotationMatrix = transpose(mult(rotateX(theta[xAxis]), mult(rotateY(theta[yAxis]), rotateZ(theta[zAxis]))));
+    var modelViewMatrix, projectionMatrix;
     var viewMatrix = lookAt(eye, at, up);
     //projectionMatrix = perspective(fovy, aspect, near, far);
     projectionMatrix = ortho(-orthoTop, orthoTop, -orthoTop, orthoTop, near, far);
 
     //set gl projection and rotation matrices, which don't change between cubies yet
     gl.uniformMatrix4fv(_projectionMatrix, false, flatten(projectionMatrix));	//TODO: allow moving closer/further from camera by changing this every render?
-    gl.uniformMatrix4fv(_rotationMatrix, false, flatten(rotationMatrix));		
 
-    var i = 0;
-    RCube.cubies.forEach(function(cubie){
-    	if(i % 2 == 0){
-    		cubie.rubiksCubeTheta = theta;
-    	}
-    	else{
-    		cubie.ownTheta = theta;
-    	}
-		i++;
-
+    rCube.cubies.forEach(function(cubie){
     	modelViewMatrix = mult(viewMatrix, cubie.getModelMatrix());
         gl.uniformMatrix4fv(_modelViewMatrix, false, flatten(modelViewMatrix));
         gl.drawArrays(gl.TRIANGLES, 0, NumVertices);  
-
     });
 
     requestAnimFrame( render );
